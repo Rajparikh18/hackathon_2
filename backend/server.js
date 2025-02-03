4727c26f18fc7727a82254501e791cb9de59a210
Rajparikh18 <parikhraj85@gmail.com>
2025-02-03 20:21:53 +0530

    dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-
 import express from 'express';
 import mongoose from 'mongoose';
 const app = express();
 const PORT = process.env.PORT || 3000;
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
 };
 
+console.log('MongoDB Username:', process.env.MONGO_USERNAME);
+console.log('MongoDB Password:', process.env.MONGO_PASSWORD);
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ye5ib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ye5ib.mongodb.net/myDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 

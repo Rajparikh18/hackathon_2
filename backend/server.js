eb7858e2154968e9552d2032e40a52e37977ce1c
vallabhdsangvikar18 <vallabhdsangvikar18@gmail.com>
2025-02-03 14:44:49 +0000

    setup going

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 const app = express();
 const PORT = process.env.PORT || 3000;
+import cors from 'cors';
 
-mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+};
+
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ye5ib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
+app.use(cors(corsOptions));
 app.get('/test', (req, res) => {
   res.send('Test route working!');
 });
